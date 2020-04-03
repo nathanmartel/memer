8efd535daa87e2016f4ba0047901941e514e5bde
Nathan Martel <nathanmartel@gmail.com>
2020-04-02 22:29:10 -0700

    CORS pt 2

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 
-const cors = require('cors');
 const app = require('./lib/app');
 
 const PORT = process.env.PORT || 7890;
 
-app.use(cors());
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Started on ${PORT}`);
